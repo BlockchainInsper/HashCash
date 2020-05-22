5c124968f60784d028728872a1352735d287e7e8
felipe <aronakamoto@outlook.com>
2020-05-22 17:18:50 -0300

    Feat: JWT process and authenticating users

--- server/report.py
+++ server/report.py
@@ -8,6 +8,11 @@ class HashReport:
         self.pd = pd
         self.create_df_time()
         self.create_df_difficulty()
+        self.create_df_user()
+    
+    def create_df_user(self):
+        data = {'id': [], 'username': [], 'password': []}
+        self.df_user = self.pd.DataFrame(data)
 
     def create_df_time(self):
         data = {'id': [], 'time': [], 'timestamp': []}
@@ -20,7 +25,7 @@ class HashReport:
     def insert_time(self, time):
         self.df_time = self.df_time.append({
             'id': str(uuid.uuid4()),
-            'time': str(time),
+            'time': time,
             'timestamp': str(datetime.datetime.utcnow())
         }, ignore_index=True)
 
@@ -29,4 +34,12 @@ class HashReport:
             'id': str(uuid.uuid4()),
             'difficulty': str(difficulty),
             'timestamp': str(datetime.datetime.utcnow())
+        }, ignore_index=True)
+
+    def insert_user(self, username, password, salt):
+        self.df_user = self.df_user.append({
+            'id': str(uuid.uuid4()),
+            'username': str(username),
+            'password': str(password),
+            'salt': str(salt)
         }, ignore_index=True)
